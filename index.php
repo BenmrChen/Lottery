0d913954b5f6756bd6e12d23805b697c96d46799
mingren.chen16 <mingren.chen16@gmail.com>
2019-05-09 10:56:26 +0800

    Initial import of Heroku for lottery

---
+++ index.php
@@ -0,0 +1,13 @@
+<h1>亂數抽獎系統</h1>
+<hr>
+
+<h2>說明</h2>
+<p>請在"範圍"欄內填入您想要抽出的最小值及最大值。</p>
+<p>例: 若您想要在1~100中隨機抽出一個數出來的話，請在最小值填入1，最大值填入100。</p>
+<form action="showResult.php">
+  <h2>範圍: </h2>
+  <p>最小值: <input type="text" name="min"></p>
+  <p>最大值: <input type="text" name="max"></p>
+  <input type="submit" value="開始抽獎">
+</form>
+
---
+++ showResult.php
@@ -0,0 +1,13 @@
+<?php 
+ if (empty($_GET['min']) || empty($_GET['max'])) {
+  $rand = rand(1,100);
+ } else {
+  $min = $_GET['min'];
+  var_dump($min);
+  $max = $_GET['max'];
+  $rand = rand("$min","$max");
+ }
+?>
+<h1>抽獎結果</h1>
+<hr> 
+<p>恭喜您，抽到的幸運得獎者為<?=$rand?>號</p>
